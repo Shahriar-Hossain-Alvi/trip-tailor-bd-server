b61ff976509088b8bd4a7eb0526ce842f17c9b7f
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-06-02 20:35:28 +0600

    added api to users data

--- index.js
+++ index.js
@@ -34,8 +34,39 @@ async function run() {
 
     const usersCollection = client.db("tripTailorBD").collection("users");
 
+    //jwt related api
+    // app.post('/jwt', async (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' });
+
+    //   res.send({ token });
+    // })
 
 
+    //save a user in the db 
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
+
+      //check if the user already exist in the db
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        if (user?.status === "requested") {
+          const result = await usersCollection.updateOne(query, { $set: { status: user?.status } })
+          return res.send(result);
+        }
+        else {
+          return res.send(isExist);
+        }
+      }
+
+      //save the user for the first time
+      const options = { upsert: true };
+      const updateDoc = { $set: { ...user } }
+
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
