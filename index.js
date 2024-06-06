15b2a3a7976429d78f9c598acfe99df47f6d73bc
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-06-06 15:32:42 +0600

    added delete api for wishlist

--- index.js
+++ index.js
@@ -243,6 +243,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/wishlist/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await wishlistCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
